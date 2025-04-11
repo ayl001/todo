ff8e2145c3960ba9a11e43a30c4d1c15cbc5fc0f
Yves LAMBERT <yves.lambert@fms-ea.com>
2025-04-11 12:48:13 +0200

    doublon

--- src/test/home.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Home from "../components/Home";
-
-test("Affiche correctement le texte", () => {
-  render(<Home theme="light" />);
-  const textElement = screen.getByText(/Bienvenue à votre liste de tâches/);
-  expect(textElement).toBeInTheDocument();
-});
